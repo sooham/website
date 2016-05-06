20c75907bdb3de57eeea23530d2a777dd0c52449
sooham <rafizsooham@gmail.com>
2016-05-06 15:06:32 -0400

    backend post and project handler done

--- scripts/router.js
+++ scripts/router.js
@@ -5,40 +5,36 @@ var Router = function() {
     this.routes = [];
 }
 
-/* Add a patten and associated handler to the router
+/* Add a patten with method and associated handler to the router
+ * @method String
  * @pattern RegExp
  * @handler Function (http.IncomingMessage, http.ServerResponse,  ...)
  */
-Router.prototype.add = function(pattern, handler) {
+Router.prototype.add = function(method, pattern, handler) {
     this.routes.push({
+        "method": method,
         "pattern": pattern,
         "handler": handler
     });
 }
 
-/* Routes a request and calls the associated function handler on
+/* Resolves a request and calls the associated function handler on
  * the request and any captured groups. returns true if the request
- * was routed correctly (matched any one of the route patterns).
+ * was routed correctly (matched any one of the route patterns and method).
  *
  * @request http.IncomingMessage
  * @response http.ServerResponse
  */
-Router.prototype.route = function(request, response) {
-    var requestURL =  request.url;
+Router.prototype.resolve = function(request, response) {
+    var path = url.parse(request.url).pathname;
+
     var isRouted = this.routes.some(function(route) {
         // TODO: Do I need to make this deal with POST requests to /admin?
-        var path = url.parse(requestURL).pathname;
         var match = route.pattern.exec(path);
-        if (match) {
+        if (match && route.method === request.method) {
             // call route.handler with request and all captured groups
-            var args = [];
-            for (var i = 0; i < match.length - 2; i++) {
-                if (match[i])
-                    args.push(decodeURIComponent(match[i]));
-            }
-            args.unshift(response);
-            args.unshift(request);
-            route.handler.apply(null, args);
+            var args = match.slice(1).map(decodeURIComponent);
+            route.handler.apply(null, [request, response].concat(args));
             return true;
         }
         return false;
--- scripts/server.js
+++ scripts/server.js
@@ -1,53 +1,115 @@
-var http = require("http");
-var url = require("url");
 var Router = require("router");
+var http = require("http");
+var querystring = require("querystring");
+var tls = require("tls");
+var url = require("url");
 
-// HTTP schema
-// GET / redirects to /blog (for now)
-// GET /blog return the JSON objects for the blog
-// GET /projects return the JSON objects for the project
-// GET /admin return the admin login page
-// POST /admin username and password to login
-
-Router.add(/^\/$/, rootHandler);
-Router.add(/^\/blog(?:\/|\/([^\/]+))?$/, blogHandler);
-Router.add(/^\/projects/?$/, projectsHandler);
-Router.add(/^\/admin/?$/, adminHandler);
-
-/* Respond with statusCode */
-function respond(response, statusCode, body, type, headers) {
+/* Respond with statusCode, data with type and headers
+ *
+ * @response http.ServerResponse
+ * @statusCode int
+ * @body String
+ * @type String
+ * @headers Object
+ */
+function respond(response, statusCode, data, type, headers) {
     var defaultHeader = {
-        "Content-Length": body.length,
+        "Content-Length": data.length,
         "Content-Type": type || "text/html"
-    }
+    };
     if (headers)
-        Object.keys(headers).forEach(function (key) {defaultHeader[key] = headers[key];})
+        Object.keys(headers).forEach(function (key) {defaultHeader[key] = headers[key];
+        });
         
     response.writeHead(statusCode, defaultHeader);
     response.write(body);
     response.end();
 }
 
+/* Respond with JSON data with statusCode and headers */
+function respondAsJSON(response, statusCode, data, headers) {
+    respond(response, statusCode, JSON.stringify(data), "application/json", headers);
+}
+
+/* Scrape key value pairs from request body 
+ * @request http.IncomingMessage
+ */
+function getKeyValuePairsFromStream(request) {
+    var body = "";
+    request.on('error') = function () {
+        // what to do here?
+        console.error("'error' occurred at getKeyValuePairsFromStream");
+    }
+
+    request.on("data") = function (chunk) {
+        body += chunk;
+    }
+    request.on("end") = function () {
+        // process and return the key value pairs here
+        return querystring.parse(body);
+    }
+}
+
+// HTTP schema
+// GET / redirects to /blog (for now)
+// GET /blog return the JSON objects for the blog
+// POST /blog posts a new blog post to blog
+// GET /projects return the JSON objects for the project
+// GET /admin return the admin login page
+// POST /admin username and password to login
+
+Router.add("GET", /^\/$/, rootHandler);
+Router.add("GET", /^\/admin\/?$/, getAdminHandler);
+Router.add("GET", /^\/blog\/([^\/]+)/?$/, getBlogPostHandler);
+Router.add("GET", /^\/blog\/?$/, getBlogPostsHandler);
+Router.add("GET", /^\/projects\/?$/, projectsHandler);
+Router.add("POST", /^\/admin\/?$/, loginHandler);
+Router.add("POST", /^\/blog\/([^\/]+)/?$/, postBlogPostHandler);
+
+/* Handles the / */
 function rootHandler(request, response) {
+    // do a redirect to /blog
+    respond(response, 302, "", null, {"Location": "/blog"});
 }
 
-function blogHandler(request, response) {
-
+/* Handles the GET /blog */
+function getBlogPostsHandler(request, response) {
+    // get JSON posts from the database
+    var blog_posts = GetPostFromMongo();
+    respondAsJSON(response, 200, blog_posts);
 }
 
+/* Handles the get /blog/post-name */
+function getBlogPostHandler(request, response) {
+    // get JSON posts from the database
+    var post_name = arguments[2];
+    var blog_post = GetPostFromMongo(post_name);
+    respondAsJSON(response, 200, blog_post);
+}
+
+/* Handles the /projects */
 function projectsHandler(request, response) {
-
+    var project_posts = GetProjectPostsFromMongo();
+    respondAsJSON(response, 200, project_posts);
 }
 
-function adminHandler(request, response) {
-
+function getAdminHandler(request, response) {
+    // make login system
 }
 
+function loginHandler(request, response) {
+    // make login system
+}
+
+// 401 - unauthorized
+// 400 - Bad Request
+// 404 - Forbidden - request is valid but server refuses to respond
+// 405 - method not allowed
 
 var server = http.createServer(function (request, response) {
-    var isRouted = Router.route(request, response);
-    if (!isRouted) {
+    if (!Router.route(request, response)) {
         // send a 404
+        respond(response, 404, http.STATUS_CODES[404],"text/plain");
     }
     response.end();
 })
@@ -55,4 +117,4 @@ var server = http.createServer(function (request, response) {
 server.on('clientError', function (err, socket) {
     socket.end("HTTP/1.1 400 Bad request\r\n\r\n");
 })
-server.listen(8080);
+server.listen(8080, "hostname");
