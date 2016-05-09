09a9c8e1287f49eade279bce4c2602ba51db164f
sooham <rafizsooham@gmail.com>
2016-05-09 18:45:31 -0400

    started work on the editor

--- scripts/database.js
+++ scripts/database.js
@@ -3,33 +3,29 @@
 var mongoose = require("mongoose");
 
 var Database = module.exports = function() {
-   this.connection = mongoose.connect(
-        "mongodb://localhost:27017/data/db",
-        { config: { autoIndex: false}}
+    this.connection = mongoose.connect(
+        "mongodb://localhost:27017/data/db", { config: { autoIndex: false } }
     );
 
     this.blogSchema = new mongoose.Schema({
-        title: {type: String, trim: true},
+        title: { type: String, trim: true },
         tags: [String],
-        date: {type: Date, default: Date.now()},
+        date: { type: Date, default: Date.now() },
         body: String,
-    }, {minimize: "false"});
-    this.blogSchema.index({ date: 1});
+    }, { minimize: "false" });
+    this.blogSchema.index({ date: 1 });
 
     this.projectsSchema = new mongoose.Schema({
-        title: {type: String, trim: true},
+        title: { type: String, trim: true },
         startDate: Date,
         technologies: [String],
-        daysOfWork: {type: Number, min: 0},
+        daysOfWork: { type: Number, min: 0 },
         description: String
-    }, {collection: "projects"});
-    this.projectsSchema.index({ title: 1});
+    }, { collection: "projects" });
+    this.projectsSchema.index({ title: 1 });
 
-    this.tagsSchema = new mongoose.Schema(
-        {name: {type: String, trim: true}},
-        {collection: "tags"}
-    );
-    this.tagsSchema.index({ name: 1});
+    this.tagsSchema = new mongoose.Schema({ name: { type: String, trim: true } }, { collection: "tags" });
+    this.tagsSchema.index({ name: 1 });
 
     this.userSchema = new mongoose.Schema({
         usermail: String,
@@ -45,23 +41,23 @@ var Database = module.exports = function() {
 };
 
 Database.prototype.findPostWithTitle = function(name, callback) {
-    this.blog.find({title: name}, callback);
+    this.blog.find({ title: name }, callback);
 };
 
-Database.prototype.findAllPosts = function (callback) {
+Database.prototype.findAllPosts = function(callback) {
     this.blog.find(callback);
 };
 
-Database.prototype.findAllProjects = function (callback) {
+Database.prototype.findAllProjects = function(callback) {
     this.projects.find(callback);
 };
 
-Database.prototype.findAllPostsWithTag = function (tag, callback) {
-    this.blog.find({tags: tag}, callback);
+Database.prototype.findAllPostsWithTag = function(tag, callback) {
+    this.blog.find({ tags: tag }, callback);
 };
 
 Database.prototype.removePostWithTitle = function(name, callback) {
-    this.blog.remove({title: name}, callback);
+    this.blog.remove({ title: name }, callback);
 };
 
 // make sure blog post does not prexist before calling
@@ -70,7 +66,7 @@ Database.prototype.savePost = function(post, callback) {
     newPost.save(callback);
 };
 
-Database.prototype.getSoohamLoginCredentials = function (callback) {
+Database.prototype.getSoohamLoginCredentials = function(callback) {
     this.user.find(callback);
 };
 
