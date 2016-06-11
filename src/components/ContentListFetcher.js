0a342ae690f3667b96a7604eaa8c7455b0db7986
sooham <rafizsooham@gmail.com>
2016-06-11 16:39:48 -0400

    Fixed loading bug when entry from non root path

--- src/components/ContentListFetcher.js
+++ src/components/ContentListFetcher.js
@@ -1,9 +1,11 @@
 import React from "react";
 import data from "utils/data";
+import $ from "jquery";
 
 import ContentListView from "components/ContentListView";
 
 import styles from "styles/dashboard.module.css";
+
 
 // TODO: move to containers/
 // TODO: add proptypes and default props and what not
@@ -24,28 +26,35 @@ export default React.createClass({
     // TODO: did a quick and dirty fix to enable animation when user
     // does not start at /, clean this up by including a state in this component
     // should these be with the parent?
+    // TODO: you'll need to pass a state down from parent to child,
+    // seems like the most effective way to fix this issue
 
-    componentDidAppear: function (cb) {
+    componentWillAppear: function (cb) {
         console.log("will appear");
-        $(`#${styles.indexNav}`).animate(
-            {width: "30%"}, 400
-            );
+        $(document).ready(() => {
+            $(`#${styles.indexNav}`).width("30%");
+            cb();
+        });
     },
 
     componentWillEnter: function(cb) {
         console.log("c will enter");
-        $(`#${styles.indexNav}`).animate(
-            {width: "30%"}, 400
-            );
-        cb();
+        $(document).ready(() => {
+            $(`#${styles.indexNav}`).animate(
+                {width: "30%"}, 400
+                );
+            cb();
+        });
     },
 
     componentWillLeave: function(cb) {
         console.log("c will leave");
-        $(`#${styles.indexNav}`).animate(
-            {width: "100%"}, 400
-            );
-        cb();
+        $(document).ready(() => {
+            $(`#${styles.indexNav}`).animate(
+                {width: "100%"}, 400
+                );
+            cb();
+        });
     },
 
     componentWillReceiveProps: function(nextProps) {
